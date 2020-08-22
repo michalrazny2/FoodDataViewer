12db6a69958afdfa324bdb505a4c843f16c498e4
mr <michalrazny2@o2.pl>
2020-08-22 18:23:08 +0200

    - food details layout added

--- app/src/main/java/com/example/fooddataviewer/fooddetails/FoodDetailsFragment.kt
+++ app/src/main/java/com/example/fooddataviewer/fooddetails/FoodDetailsFragment.kt
@@ -2,11 +2,12 @@ package com.example.fooddataviewer.fooddetails
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.fooddataviewer.R
 import com.example.fooddataviewer.getViewModel
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 
-class FoodDetailsFragment : Fragment() {
+class FoodDetailsFragment : Fragment(R.layout.food_details_fragment) {
 
     private val args: FoodDetailsFragmentArgs by navArgs()
     private lateinit var disposable: Disposable
